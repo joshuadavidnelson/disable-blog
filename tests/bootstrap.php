c9caddeaa0307d2a4531fcbc12aae1034a8bf735
Joshua Nelson <josh@joshuadnelson.com>
2020-12-27 16:14:13 +0000

    initial test scaffold

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,32 @@
 /**
  * Bootstrap file for tests.
  *
- * @since 0.4.9
  * @package Disable_Blog
  */
 
 define( 'DWPB_VERSION', '0.5.0' );
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/disable-blog.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
