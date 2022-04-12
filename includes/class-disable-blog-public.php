34dc0863bf5eea37b524ae8fd9f99bd8b4a0d38a
Joshua Nelson <josh@joshuadnelson.com>
2022-04-12 08:08:52 -0400

    phpcs ignore ternary operator to avoid phpstan error
    bool value can be passed via filter

--- includes/class-disable-blog-public.php
+++ includes/class-disable-blog-public.php
@@ -477,7 +477,7 @@ class Disable_Blog_Public {
 		$methods_to_remove = apply_filters( 'dwpb_disabled_xmlrpc_methods', $methods_to_remove );
 
 		// filter any invalid entries out before returning the array.
-		return is_array( $methods_to_remove ) ? array_filter( $methods_to_remove, 'is_string' ) : false;
+		return is_array( $methods_to_remove ) ? array_filter( $methods_to_remove, 'is_string' ) : false; // phpcs:ignore
 
 	}
 
