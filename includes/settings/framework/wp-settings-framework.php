7d613667cb7682290b4a1c9d72a0f490934ce6c3
Joshua Nelson <josh@joshuadnelson.com>
2022-04-21 08:51:05 -0400

    fix framework toggle field label so it's readable

--- includes/settings/framework/wp-settings-framework.php
+++ includes/settings/framework/wp-settings-framework.php
@@ -731,7 +731,7 @@ if ( ! class_exists( 'WordPressSettingsFramework' ) ) {
 			$checked       = ( $args['value'] ) ? 'checked="checked"' : '';
 
 			echo '<input type="hidden" name="' . $args['name'] . '" value="0" />';
-			echo '<label class="switch"><input type="checkbox" name="' . $args['name'] . '" id="' . $args['id'] . '" value="1" class="' . $args['class'] . '" ' . $checked . '> ' . $args['desc'] . '<span class="slider"></span></label>';
+			echo '<label class="switch"><input type="checkbox" name="' . $args['name'] . '" id="' . $args['id'] . '" value="1" class="' . $args['class'] . '" ' . $checked . '> <span class="slider"></span></label><p  class="description" id="label-text-' . $args['id'] . '"><label for="' . $args['name'] . '">' . $args['desc'] . '</label></p>';
 		}
 
 		/**
