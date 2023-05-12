f4180037efd00afacebb498d6800b82cb074e1ae
Joshua Nelson <josh@joshuadnelson.com>
2023-05-12 15:16:30 -0400

    use a function to test redirect request & response

--- tests/cypress/e2e/redirects.test.js
+++ tests/cypress/e2e/redirects.test.js
@@ -1,11 +1,25 @@
+const testRedirect = (origin, expectedUrl = "", responseCode = 301) => {
+
+	// Append the base url to the expected url.
+	expectedUrl = Cypress.config().baseUrl + expectedUrl;
+
+	// Test the redirect.
+	return cy.request({
+		url: origin,
+		followRedirect: false, // turn off following redirects
+	}).then((resp) => {
+		expect(resp.status).to.eq(responseCode);
+		expect(resp.redirectedToUrl).to.eq(expectedUrl);
+	});
+};
+
 describe("URL redirects work when plugin is active", () => {
 	before(() => {
 
 	});
 
 	it("Post redirects to homepage", () => {
-		cy.visit("/hello-world/");
-		cy.wait(2000);
-		cy.location("pathname").should("eq", "/");
+		testRedirect("/hello-world/");
+	});
 	});
 });
