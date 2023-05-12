820282b8ddba46a8eaea527511b69e82ca0cb9b1
Joshua Nelson <josh@joshuadnelson.com>
2023-05-12 13:30:06 -0400

    remove blog redirect test for now

--- tests/cypress/e2e/redirects.test.js
+++ tests/cypress/e2e/redirects.test.js
@@ -1,12 +1,6 @@
 describe("URL redirects work when plugin is active", () => {
 	before(() => {
-		cy.login();
-	});
 
-	it("Blog page redirects to homepage", () => {
-		cy.visit("/blog/");
-		cy.wait(2000);
-		cy.location("pathname").should("eq", "/");
 	});
 
 	it("Post redirects to homepage", () => {
