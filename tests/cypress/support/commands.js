596f4115a25425704fe74e0486187c7c8b74683b
Joshua Nelson <josh@joshuadnelson.com>
2023-05-15 11:16:34 -0400

    Create sitemap.test.js

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -13,9 +13,27 @@ Cypress.Commands.add('testRedirect', (origin, expectedUrl = "", responseCode = 3
 	});
 });
 
+// Test that a url returns a specific response code.
+Cypress.Commands.add('urlReturns', (origin, responseCode = 200, followRedirect=false) => {
+	return cy.request({
+		url: origin,
+		followRedirect: followRedirect,
+		failOnStatusCode: false,
+	}).then((resp) => {
+		expect(resp.status).to.eq(responseCode);
+	});
+});
+
 Cypress.Commands.add('setUpPlugin', () => {
 	cy.login();
 	cy.deactivateAllPlugins();
 	cy.activatePlugin('disable-blog');
+});
+
+Cypress.Commands.add('checkUrls', (urls, responseCode = 200) => {
+	urls.forEach((url) => {
+		cy.log(url);
+		cy.urlReturns(url, responseCode);
+	});
 });
 
