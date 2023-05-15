44cec1e6ff736b8cde82a50d8172f60b74acdaba
Joshua Nelson <josh@joshuadnelson.com>
2023-05-15 09:25:40 -0400

    update redirect tests to use custom command

--- tests/cypress/e2e/redirects.test.js
+++ tests/cypress/e2e/redirects.test.js
@@ -1,17 +1,3 @@
-const testRedirect = (origin, expectedUrl = "", responseCode = 301) => {
-
-	// Append the base url to the expected url.
-	expectedUrl = Cypress.config().baseUrl + expectedUrl;
-
-	// Test the redirect.
-	return cy.request({
-		url: origin,
-		followRedirect: false, // turn off following redirects
-	}).then((resp) => {
-		expect(resp.status).to.eq(responseCode);
-		expect(resp.redirectedToUrl).to.eq(expectedUrl);
-	});
-};
 
 describe("Front-end redirects work when plugin is active", () => {
 	beforeEach(() => {
@@ -23,19 +9,19 @@ describe("Front-end redirects work when plugin is active", () => {
 	});
 
 	it("Category redirects to homepage", () => {
-		testRedirect("/category/uncategorized/");
+		cy.testRedirect("/category/uncategorized/");
 	});
 
 	it("Date redirects to homepage", () => {
 		const dayjs = require('dayjs');
 		const dateArchiveSlug = dayjs().format('/YYYY/MM/');
-		testRedirect(dateArchiveSlug);
+		cy.testRedirect(dateArchiveSlug);
 	});
 
 	it("Author redirects to homepage", () => {
 		cy.login();
 		cy.activatePlugin('author-archive-filter');
-		testRedirect("/author/admin/");
+		cy.testRedirect("/author/admin/");
 		cy.deactivatePlugin('author-archive-filter');
 	});
 
@@ -43,36 +29,35 @@ describe("Front-end redirects work when plugin is active", () => {
 
 describe("Admin redirects work when plugin is active", () => {
 	beforeEach(() => {
-		cy.login();
+		cy.setUpPlugin();
 	});
 
 	it("Posts edit page redirects to dashboard", () => {
-		testRedirect("/wp-admin/post.php?post=1&action=edit","wp-admin/");
+		cy.testRedirect("/wp-admin/post.php?post=1&action=edit","wp-admin/");
 	});
 
 	it("Posts admin screen redirects to pages admin screen", () => {
-		testRedirect("/wp-admin/edit.php?post_type=post","wp-admin/edit.php?post_type=page");
+		cy.testRedirect("/wp-admin/edit.php?post_type=post","wp-admin/edit.php?post_type=page");
 	});
 
 	it("New post screen redirects to new page screen", () => {
-		testRedirect("/wp-admin/post-new.php?post_type=post","wp-admin/post-new.php?post_type=page");
+		cy.testRedirect("/wp-admin/post-new.php?post_type=post","wp-admin/post-new.php?post_type=page");
 	});
 
 	// Category edit and term new pages redirect to dashboard.
 	it("Category edit page redirects to dashboard", () => {
-		testRedirect("/wp-admin/edit-tags.php?taxonomy=category","wp-admin/");
+		cy.testRedirect("/wp-admin/edit-tags.php?taxonomy=category","wp-admin/");
 	});
 
 	// Tag edit and tag new pages redirect to dashboard.
 	it("Tag edit page redirects to dashboard", () => {
-		testRedirect("/wp-admin/edit-tags.php?taxonomy=post_tag","wp-admin/");
+		cy.testRedirect("/wp-admin/edit-tags.php?taxonomy=post_tag","wp-admin/");
 	});
 
 	// Redirect writing options to options general.
-	it("Writing options redirects to general options", () => {
-		cy.login();
+	it("Writing options redirects to general options with filter", () => {
 		cy.activatePlugin('remove-options-writing');
-		testRedirect("/wp-admin/options-writing.php","wp-admin/options-general.php");
+		cy.testRedirect("/wp-admin/options-writing.php","wp-admin/options-general.php");
 		cy.deactivatePlugin('remove-options-writing');
 	});
 });
--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -1,1 +1,21 @@
+Cypress.Commands.add('testRedirect', (origin, expectedUrl = "", responseCode = 301) => {
+
+	// Append the base url to the expected url.
+	expectedUrl = Cypress.config().baseUrl + expectedUrl;
+
+	// Test the redirect.
+	return cy.request({
+		url: origin,
+		followRedirect: false, // turn off following redirects
+	}).then((resp) => {
+		expect(resp.status).to.eq(responseCode);
+		expect(resp.redirectedToUrl).to.eq(expectedUrl);
+	});
+});
+
+Cypress.Commands.add('setUpPlugin', () => {
+	cy.login();
+	cy.deactivateAllPlugins();
+	cy.activatePlugin('disable-blog');
+});
 
