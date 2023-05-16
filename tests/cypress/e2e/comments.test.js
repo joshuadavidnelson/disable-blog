6b72b2cbb05a08840bf36b415315b8d7b367cc2e
Joshua Nelson <josh@joshuadnelson.com>
2023-05-16 07:43:10 -0400

    flush out the comment test

--- tests/cypress/e2e/comments.test.js
+++ tests/cypress/e2e/comments.test.js
@@ -1,12 +1,18 @@
 describe("Test comment related stuff", () => {
-	beforeEach(() => {
-		cy.setUpPlugin();
-	});
 
-	it("No comments in default site with plugin active", () => {
+	it("Post comments disappear when plugin is active", () => {
+		cy.login();
+		cy.deactivateAllPlugins();
+		cy.visit("/wp-admin/edit-comments.php");
+		cy.wait(2000);
+		cy.get("#the-comment-list").should('not.include.text', 'No comments found.');
+		cy.get("#the-comment-list #comment-1").should('exist');
+
+		cy.setUpPlugin();
 		cy.visit("/wp-admin/edit-comments.php");
 		cy.wait(2000);
 		cy.get("#the-comment-list").contains("No comments found.");
+		cy.get("#the-comment-list #comment-1").should('not.exist');
 	});
 
 });
